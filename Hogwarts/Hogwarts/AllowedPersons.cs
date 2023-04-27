9a4409fd654a4350f0fae1100ea2b60e60a2a242
Mahdi Rahbar <m.rahbar.1102@gmail.com>
2023-04-28 02:47:24 +0330

    creating Team class and compliting properties of AllowedPersons class.

--- Hogwarts/Hogwarts/AllowedPersons.cs
+++ Hogwarts/Hogwarts/AllowedPersons.cs
@@ -7,26 +7,26 @@ using System.IO;
 using Newtonsoft.Json;
 namespace Hogwarts
 {
-    enum Pet
+    enum Animal
     {
         Rat,
         Cat,
         Owl
     };
+    enum Role
+    {
+        Teacher,
+        Student
+    };
     class AllowedPersons : Human
     {
         public String RoomNumber { get; set; }
         public String SchoolTable { get; set; }
-        public Pet Animal { get; set; }
-
-
-
-
-
-
-
-
-
+        public Animal Pet { get; set; }
+        public Team Group { get; set; }
+        public bool HaveSuitcase { get; set; }
+        public Role E_Role { get; set; }
+        public string[] Letter { get; set; }
         public List<Human> LoadJson()
         {
             using (StreamReader file = new StreamReader(@"E:\Daneshgah\Advanced Programming\2\Hogwarts Project\mini_project2_hogwarts-Mahdi-Rahbar\Files\JSON_DATA.json"))
---
+++ Hogwarts/Hogwarts/Team.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hogwarts
+{
+    enum Type
+    {
+        Hufflepuff,
+        Gryffindor,
+        Ravenclaw,
+        Slytherin
+    };
+    class Team
+    {
+        public Type TeamType { get; set; }
+        public int Point { get; set; }
+        public List<AllowedPersons> team_members = new List<AllowedPersons>();
+        public List<AllowedPersons> quidditch_members = new List<AllowedPersons>();
+    }
+}
